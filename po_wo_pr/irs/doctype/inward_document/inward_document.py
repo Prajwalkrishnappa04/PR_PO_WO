037d3499402ba6a93e2806bbb9491cb8df709cd8
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-04 12:23:17 +0530

    irs module added

---
+++ po_wo_pr/irs/doctype/inward_document/inward_document.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025, Hybrowlabs and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class InwardDocument(Document):
+	#to save entry by user
+	def save_entry_By(self):
+		if not self.entry_by:
+			self.entry_by = frappe.session.user
+
+	#before save hook
+	def before_save(self):
+		self.save_entry_By()
