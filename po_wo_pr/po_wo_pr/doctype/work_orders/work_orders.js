7866c15afe10910510cbc724f855596f4e6885a0
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-05 14:36:52 +0530

    calculation of tax and discount is also added

--- po_wo_pr/po_wo_pr/doctype/work_orders/work_orders.js
+++ po_wo_pr/po_wo_pr/doctype/work_orders/work_orders.js
@@ -1,3 +1,5 @@
+// ---------------- CHILD TABLE CALCULATION ---------------- //
+
 frappe.ui.form.on("Work Order Description", {
     order_quantity(frm, cdt, cdn) {
         calculate_amount(frm, cdt, cdn);
@@ -10,13 +12,107 @@ frappe.ui.form.on("Work Order Description", {
 function calculate_amount(frm, cdt, cdn) {
     let row = frappe.get_doc(cdt, cdn);
 
-    row.amount = (row.order_quantity || 0) * (row.rate || 0);
+    frappe.model.set_value(cdt, cdn, "amount",
+        (row.order_quantity || 0) * (row.rate || 0)
+    );
 
-    frm.refresh_field("description");
+    calculate_total_amount(frm);
 }
 
-frappe.ui.form.on("Work Orders", {
-    refresh(frm) {
+function calculate_total_amount(frm) {
+    let total_amount = 0;
+    (frm.doc.description || []).forEach(row => {
+        total_amount += row.amount || 0;
+    });
 
+    frm.set_value("total_value", total_amount);
+    calculate_grand_total(frm);
+}
+
+
+// ---------------- DISCOUNT LOGIC ---------------- //
+
+function calculate_discount_value(frm) {
+    if (frm._updating_discount_value) return;
+
+    let total = frm.doc.total_value || 0;
+    let percentage = frm.doc.discounted_percentage_ || 0;
+
+    frm._updating_discount_percentage = true;
+    frappe.model.set_value(frm.doc.doctype, frm.doc.name,
+        "discounted_value", (total * percentage) / 100
+    ).then(() => {
+        frm._updating_discount_percentage = false;
+        calculate_grand_total(frm);
+    });
+}
+
+function calculate_discount_percentage(frm) {
+    if (frm._updating_discount_percentage) return;
+
+    let total = frm.doc.total_value || 0;
+    let value = frm.doc.discounted_value || 0;
+
+    frm._updating_discount_value = true;
+    frappe.model.set_value(frm.doc.doctype, frm.doc.name,
+        "discounted_percentage_", (value / total) * 100
+    ).then(() => {
+        frm._updating_discount_value = false;
+        calculate_grand_total(frm);
+    });
+}
+
+
+// ---------------- GRAND TOTAL (AFTER DISCOUNT) ---------------- //
+
+function calculate_grand_total(frm) {
+    let total = frm.doc.total_value || 0;
+    let discount = frm.doc.discounted_value || 0;
+    frm.set_value("revised_value", total - discount);
+
+    calculate_taxes(frm);   // 👈 calculate all taxes
+}
+
+
+// ---------------- TAX CALCULATIONS ---------------- //
+
+function calculate_taxes(frm) {
+    let taxable = frm.doc.revised_value || 0;
+
+    let cgst = taxable * (frm.doc.cgst_ || 0) / 100;
+    let sgst = taxable * (frm.doc.sgst_ || 0) / 100;
+    let igst = taxable * (frm.doc.igst_ || 0) / 100;
+
+    frm.set_value("final_amount", taxable + cgst + sgst + igst);
+}
+
+
+
+// ---------------- FORM TRIGGERS ---------------- //
+
+frappe.ui.form.on("Work-Orders", {
+
+    discounted_percentage_(frm) {
+        calculate_discount_value(frm);
     },
+
+    discounted_value(frm) {
+        calculate_discount_percentage(frm);
+    },
+
+    cgst_(frm) {
+        calculate_taxes(frm);
+    },
+
+    sgst_(frm) {
+        calculate_taxes(frm);
+    },
+
+    igst_(frm) {
+        calculate_taxes(frm);
+    },
+
+    revised_value(frm) {
+        calculate_taxes(frm);
+    }
 });
