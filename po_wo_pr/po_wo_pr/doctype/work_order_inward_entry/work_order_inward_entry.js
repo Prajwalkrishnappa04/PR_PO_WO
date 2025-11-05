b8f29991fc7569a545e0e451af2bcd3a3fcab411
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-05 15:24:06 +0530

    added workorder inward

--- po_wo_pr/po_wo_pr/doctype/work_order_inward_entry/work_order_inward_entry.js
+++ po_wo_pr/po_wo_pr/doctype/work_order_inward_entry/work_order_inward_entry.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2025, Hybrowlabs and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Work Order Inward Entry", {
-// 	refresh(frm) {
+frappe.ui.form.on("Work Order Inward Entry", {
+    supplier_name(frm) {
+        frm.set_query("work_order_no", () => {
+            return {
+                filters: {
+                    supplier_name: frm.doc.supplier_name
+                }
+            };
+        });
+    },
+    work_order_no(frm) {
+        if (frm.doc.work_order_no) {
+            frappe.db.get_value("Work-Orders", frm.doc.work_order_no, "supplier_name", (r) => {
+                if (r && r.supplier_name) {
+                    frm.set_value("supplier_name", r.supplier_name);
+                }
+            });
+        }
+    },
+    refresh(frm) {
 
-// 	},
-// });
+    },
+});
