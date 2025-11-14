95390a0457f88a9969572056d9c0099b9bcdd2fc
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-14 10:29:38 +0530

    new change in inward to outward entry

--- po_wo_pr/irs/api.py
+++ po_wo_pr/irs/api.py
@@ -1,11 +1,16 @@
 import frappe
 @frappe.whitelist()
-def bulk_inward_to_outward(docnames):
-    print("*****************bulk_inward_to_outward called****************")
-    print(docnames)
-    docnames = frappe.parse_json(docnames)
-    for docname in docnames:
-        doc = frappe.get_doc("Inward Document", docname)
-        frappe.new_doc("Outward Documents").update({
-            "inward": doc.name,
-        }).insert()
+def bulk_inward_to_outward(docnames,extra_data=None):
+    try:
+        docnames = frappe.parse_json(docnames)
+        for docname in docnames:
+            doc = frappe.get_doc("Inward Document", docname)
+            frappe.new_doc("Outward Documents").update({
+                "inward": doc.name,
+                "doc_no": doc.doc_name,
+                "url": doc.postal_url,
+                "date": doc.date,
+            }).insert()
+        return "ok"
+    except Exception as e:
+        return str(e)
--- po_wo_pr/public/js/inward_list.js
+++ po_wo_pr/public/js/inward_list.js
@@ -2,23 +2,53 @@ frappe.listview_settings['Inward Document'] = {
     onload(listview) {
         listview.page.add_action_item(__("Create Outward Action"), function () {
             let selected = listview.get_checked_items(true);
+
             if (!selected.length) {
                 frappe.msgprint(__("Please select at least one document."));
                 return;
             }
-            console.log("selected:", selected)
-            frappe.call({
-                method: "po_wo_pr.irs.api.bulk_inward_to_outward",
-                args: {
-                    docnames: selected
-                },
-                callback: function (r) {
-                    if (r.message) {
-                        frappe.msgprint(__("Outward documents created successfully."));
-                        listview.refresh();
+
+            // Open a dialog popup
+            let d = new frappe.ui.Dialog({
+                title: __("Create Outward Action"),
+                fields: [
+                    {
+                        label: "Document Name",
+                        fieldname: "doc_name",
+                        fieldtype: "Data"
+                    },
+                    {
+                        label: "Postal Service URL",
+                        fieldname: "postal_url",
+                        fieldtype: "Data"
+                    },
+                    {
+                        label: "Postal Date",
+                        fieldname: "date",
+                        fieldtype: "Date"
                     }
+                ],
+                primary_action_label: __("Submit"),
+                primary_action(values) {
+                    // Call backend method here
+                    frappe.call({
+                        method: "po_wo_pr.irs.api.bulk_inward_to_outward",
+                        args: {
+                            docnames: selected,
+                            extra_data: values
+                        },
+                        callback(r) {
+                            if (r.message) {
+                                frappe.msgprint(__("Outward documents created successfully."));
+                                listview.refresh();
+                                d.hide();
+                            }
+                        }
+                    });
                 }
             });
+
+            d.show();
         });
     }
 };
