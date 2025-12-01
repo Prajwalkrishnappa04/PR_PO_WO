5fc14b906fd1cc40d85e343288bb4a5d7361e408
alik02-laha <alik@mail.hybrowlabs.com>
2025-12-01 17:27:42 +0530

    po customizaton done

--- po_wo_pr/public/js/purchase_order.js
+++ po_wo_pr/public/js/purchase_order.js
@@ -9,5 +9,19 @@ frappe.ui.form.on("Purchase Order", {
             },
             "Create"
         );
+    },
+    transaction_date(frm) {
+        if (frm.doc.transaction_date) {
+            let tran = frappe.datetime.str_to_obj(frm.doc.transaction_date);
+            let week_later = frappe.datetime.add_days(tran, 7);
+            frm.set_value("schedule_date", frappe.datetime.obj_to_str(week_later));
+        }
+    },
+    onload(frm) {
+        if (frm.doc.transaction_date && !frm.doc.schedule_date) {
+            let tran = frappe.datetime.str_to_obj(frm.doc.transaction_date);
+            let week_later = frappe.datetime.add_days(tran, 7);
+            frm.set_value("schedule_date", frappe.datetime.obj_to_str(week_later));
+        }
     }
 });
