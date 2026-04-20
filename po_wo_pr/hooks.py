fb7119309647265ce80ac6c81b759492875b3b3e
alik02-laha <alik@mail.hybrowlabs.com>
2026-04-20 10:22:48 +0530

    added checkin validations

--- po_wo_pr/hooks.py
+++ po_wo_pr/hooks.py
@@ -293,7 +293,7 @@ fixtures = [
                 "Employee-custom_total_loan_balance","Employee-custom_reporting_manager","Employee-custom_reporting_manager_name","Employee-custom_office_document","Employee-custom_academic_documents_list",
                 "Material Request-custom_maa_project","Employee-custom_personal_document","Employee-custom_maa_foundation_experience","Employee-custom_loan","Employee-custom_loan_amount","Employee-custom_paid_amount",
                 "Employee-custom_remaining_amount","Material Request-custom_person_responsible","Material Request-custom_for","Purchase Order Item-custom_gst_included","Expense Claim-custom_maa_project","Attendance-custom_work_hours",
-                "Employee Checkin-custom_exact_time","Employee Checkin-custom_exact_date",
+                "Employee Checkin-custom_exact_time","Employee Checkin-custom_exact_date","Employee Checkin-custom_branch","Attendance-custom_branch"
                 
             ]
             ]
