6d483df016078a073f0ca64a2d98382f543d0a70
alik02-laha <alik@mail.hybrowlabs.com>
2026-04-30 13:13:14 +0530

    fixed rediract issue

--- po_wo_pr/public/js/attendance_list.js
+++ po_wo_pr/public/js/attendance_list.js
@@ -1,3 +1,23 @@
+console.log('Attendance List JS loaded');
+
+const redirect_attendance_report = () => {
+    const route = frappe.get_route();
+    if (route[0] === 'List' && route[1] === 'Attendance' && route[2] === 'Report') {
+        console.log('Redirecting to Attendance Custom Report...');
+        frappe.set_route('query-report', 'Attendance Custom Report');
+    }
+};
+
+// Catch direct navigation or route changes immediately
+frappe.router.on('change', () => {
+    redirect_attendance_report();
+});
+
+// Also check immediately upon script load
+$(document).ready(() => {
+    redirect_attendance_report();
+});
+
 if (!frappe.listview_settings['Attendance']) {
     frappe.listview_settings['Attendance'] = {};
 }
@@ -10,24 +30,14 @@ Object.assign(frappe.listview_settings['Attendance'], {
         if (old_attendance_onload) {
             old_attendance_onload.call(this, listview);
         }
-        this.redirect_to_custom_report(listview);
+        redirect_attendance_report();
+        this.setup_redirect_interceptor(listview);
     },
     refresh: function(listview) {
-        this.redirect_to_custom_report(listview);
+        redirect_attendance_report();
     },
-    redirect_to_custom_report: function(listview) {
-        const redirect = () => {
-            const route = frappe.get_route();
-            if (route[0] === 'List' && route[1] === 'Attendance' && route[2] === 'Report') {
-                console.log('Redirecting to Attendance Custom Report...');
-                frappe.set_route('query-report', 'Attendance Custom Report');
-            }
-        };
-
-        // 1. Initial check (if landed directly on Report view)
-        redirect();
-
-        // 2. Intercept sidebar view switches (interceptor for UI actions)
+    setup_redirect_interceptor: function(listview) {
+        // Intercept sidebar view switches (interceptor for UI actions)
         if (!listview.set_view_overridden) {
             const original_set_view = listview.set_view;
             listview.set_view = function (view) {
@@ -38,14 +48,6 @@ Object.assign(frappe.listview_settings['Attendance'], {
                 return original_set_view.call(this, view);
             };
             listview.set_view_overridden = true;
-        }
-
-        // 3. Catch direct URL changes or other navigation methods (Router Hook)
-        if (!window.attendance_route_listener_set) {
-            frappe.router.on('change', () => {
-                redirect();
-            });
-            window.attendance_route_listener_set = true;
         }
     }
 });
