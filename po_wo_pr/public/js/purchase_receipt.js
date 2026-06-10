b75bd4718d731b55c104188df1785b1554a38770
Nilesh <nileshmadhukarrathod111@gmail.com>
2026-06-10 14:55:56 +0530

    solved error mrn_

--- po_wo_pr/public/js/purchase_receipt.js
+++ po_wo_pr/public/js/purchase_receipt.js
@@ -1,8 +1,28 @@
 frappe.ui.form.on("Purchase Receipt", {
     refresh(frm) {
+        set_transport_section_width(frm);
+
         frm.remove_custom_button("Landed Cost Voucher", "Create");
         setTimeout(() => {
             $(frm.wrapper).find('[data-label="Landed%20Cost%20Voucher"]').hide();
         }, 500);
     }
 });
+
+function set_transport_section_width(frm) {
+    const style_id = "po-wo-pr-purchase-receipt-transport-style";
+
+    if (!$(`#${style_id}`).length) {
+        $("head").append(`
+            <style id="${style_id}">
+                .po-wo-pr-compact-transport .section-body {
+                    max-width: 1040px;
+                }
+            </style>
+        `);
+    }
+
+    frm.wrapper
+        .find('.form-section[data-fieldname="custom_transport"]')
+        .addClass("po-wo-pr-compact-transport");
+}
