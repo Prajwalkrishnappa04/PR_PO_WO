0a9816cabfe50a400bf80c9b5a735f7e2c898e53
Nilesh <nileshmadhukarrathod111@gmail.com>
2026-06-11 10:33:54 +0530

    po,piand pr changes

--- po_wo_pr/public/js/material_request.js
+++ po_wo_pr/public/js/material_request.js
@@ -1,35 +1,54 @@
-frappe.ui.form.on("Material Request Item", {
-    item_code: function(frm, cdt, cdn) {
-        fetch_stock_balance(frm, cdt, cdn);
+frappe.ui.form.on("Material Request", {
+    refresh(frm) {
+        update_all_stock_balances(frm);
     },
 
-    warehouse: function(frm, cdt, cdn) {
-        fetch_stock_balance(frm, cdt, cdn);
+    set_warehouse(frm) {
+        update_all_stock_balances(frm);
+    },
+
+    set_from_warehouse(frm) {
+        update_all_stock_balances(frm);
     }
 });
 
-function fetch_stock_balance(frm, cdt, cdn) {
-    let row = locals[cdt][cdn];
-    console.log("row data ------------", row);
+frappe.ui.form.on("Material Request Item", {
+    item_code: function(frm, cdt, cdn) {
+        update_stock_balance(frm, cdt, cdn);
+    },
 
-    if (row.item_code && row.warehouse) {
-        frappe.call({
-            method: "erpnext.stock.utils.get_stock_balance",
-            args: {
-                item_code: row.item_code,
-                warehouse: row.warehouse
-            },
-            callback: function(r) {
-                console.log("stock balance response --------", r);
-                if (r.message !== undefined) {
-                    frappe.model.set_value(
-                        cdt,
-                        cdn,
-                        "custom_stock_balance_qty",
-                        r.message
-                    );
-                }
-            }
-        });
+    warehouse: function(frm, cdt, cdn) {
+        update_stock_balance(frm, cdt, cdn);
+    },
+
+    from_warehouse: function(frm, cdt, cdn) {
+        update_stock_balance(frm, cdt, cdn);
     }
+});
+
+function update_all_stock_balances(frm) {
+    (frm.doc.items || []).forEach(row => {
+        update_stock_balance(frm, row.doctype, row.name);
+    });
+}
+
+function update_stock_balance(frm, cdt, cdn) {
+    const row = locals[cdt][cdn];
+
+    if (!row || !row.item_code) {
+        frappe.model.set_value(cdt, cdn, "custom_stock_balance_qty", 0);
+        return;
+    }
+
+    frappe.call({
+        method: "po_wo_pr.api.setup.get_item_stock_balance",
+        args: {
+            item_code: row.item_code,
+            warehouse: row.warehouse || row.from_warehouse || frm.doc.set_warehouse || frm.doc.set_from_warehouse,
+            company: frm.doc.company
+        },
+        callback(r) {
+            frappe.model.set_value(cdt, cdn, "custom_stock_balance_qty", flt(r.message));
+        }
+    });
 }
