14abbaf9b6ca008c406d26167efef88ea9a38e32
prathameshjadhav <prathamjadhav052@gmail.com>
2026-06-22 13:20:44 +0000

    feat(inward-document): add place auto-fill and link sender to Student

--- po_wo_pr/irs/doctype/inward_document/inward_document.py
+++ po_wo_pr/irs/doctype/inward_document/inward_document.py
@@ -5,6 +5,19 @@ import frappe
 from frappe.model.document import Document
 
 
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def search_student(doctype, txt, searchfield, start, page_len, filters):
+	return frappe.db.sql(
+		"""SELECT name, student_name
+		FROM `tabStudent`
+		WHERE (name LIKE %(txt)s OR student_name LIKE %(txt)s)
+		ORDER BY student_name
+		LIMIT %(start)s, %(page_len)s""",
+		{"txt": f"%{txt}%", "start": start, "page_len": page_len},
+	)
+
+
 class InwardDocument(Document):
 	#to save entry by user
 	def save_entry_By(self):
