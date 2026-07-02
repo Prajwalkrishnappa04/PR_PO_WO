4c55c68c03177ba0b93863533f219a8e4d1063e7
prathameshjadhav <prathamjadhav052@gmail.com>
2026-07-02 13:47:35 +0000

    Fix some bugs

--- po_wo_pr/irs/doctype/outward_documents/outward_documents.py
+++ po_wo_pr/irs/doctype/outward_documents/outward_documents.py
@@ -10,6 +10,15 @@ class OutwardDocuments(Document):
 		if not self.entry_by:
 			self.entry_by = frappe.session.user
 
-	#before save hook
 	def before_save(self):
 		self.save_entry_By()
+
+	def on_trash(self):
+		# Legacy docs with plain numeric names (created before naming_series was set)
+		# cause revert_series_if_last to crash because doc.name is an int.
+		# Clearing naming_series prevents Frappe from attempting the series revert.
+		try:
+			int(self.name)
+			self.naming_series = None
+		except (ValueError, TypeError):
+			pass
