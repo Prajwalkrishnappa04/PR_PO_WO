d71e99186330877063cd9e674e4a05768e3539c4
Hemal Brahmkhatri <hemal@mail.hybrowlabs.com>
2026-08-12 22:03:23 +0530

    Refactor transport options and remove unused mode_of_transport_data files

--- po_wo_pr/po_wo_pr/doctype/mode_of_transport_data/test_mode_of_transport_data.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Hybrowlabs and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestModeOfTransportData(FrappeTestCase):
-	pass
